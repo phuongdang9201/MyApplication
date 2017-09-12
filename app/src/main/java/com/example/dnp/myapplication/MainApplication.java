8ee426eaad9f4c7147d8fe9385be4a7d68d26acd
cobala9211 <dnphuong9211@gmail.com>
2017-09-12 22:51:48 +0700

    First commit

---
+++ app/src/main/java/com/example/dnp/myapplication/MainApplication.java
@@ -0,0 +1,14 @@
+package com.example.dnp.myapplication;
+
+import android.app.Application;
+
+/**
+ * Created by DNP on 9/12/2017.
+ */
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
